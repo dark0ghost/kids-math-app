c568282396c0a8cfe2e9b20d10c0dfc4d3690e1f
dark0ghost <dark0ghostworkmail@seznam.cz>
2021-01-29 00:51:44 +0300

    Math clas

--- app/src/main/java/com/dark0ghost/math_for_kids/MathGenerate.kt
+++ app/src/main/java/com/dark0ghost/math_for_kids/MathGenerate.kt
@@ -2,18 +2,35 @@ package com.dark0ghost.math_for_kids
 
 import kotlin.random.Random
 
-class MathGenerate<T> {
-    private fun get_anwser(){
+open class MathGenerate {
+    private val validOperation: List<String> = listOf("+","-","*","/")
 
-    }
-
-    private fun generate_two_number(begin: Int,end: Int):List<Int> {
-        val result = mutableListOf<Int>()
-        result.add(Random.nextInt(begin,end))
-        result.add(Random.nextInt(begin,end))
+    private fun generateMathExample(operation: List<String>, arrayNumber: List<Int>): String{
+        var result: String = arrayNumber.random().toString()
+        for(i in arrayNumber.indices)
+            result += operation.random(Random) + arrayNumber.random(Random).toString()
         return result
     }
-    fun get_data(){
 
+
+
+    private fun generateNumberArray(begin: Int,end: Int,len: Int): List<Int> {
+        val result = mutableListOf<Int>()
+        for (_i in 0 until len)
+            result.add(Random.nextInt(begin,end))
+        return result
     }
+
+    private fun getAnswerOnExample(example: String): Int = -1
+
+    open fun getData(operation: List<String>, begin: Int, end: Int):Pair<String,Int>{
+        val operationValid = operation.filter {
+            it in validOperation
+        }
+        val numberArray = generateNumberArray(begin,end,operationValid .size * 2)
+        val mathExample = generateMathExample(operationValid ,numberArray)
+        val answer = getAnswerOnExample(mathExample)
+        return Pair(mathExample,answer)
+    }
+
 }
