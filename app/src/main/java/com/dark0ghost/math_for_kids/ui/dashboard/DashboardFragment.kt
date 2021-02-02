c2252d010edfe61726840d3d517eb299f713527b
dark0ghost <dark0ghostworkmail@seznam.cz>
2021-02-02 22:39:57 +0300

    intarface

--- app/src/main/java/com/dark0ghost/math_for_kids/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/dark0ghost/math_for_kids/ui/dashboard/DashboardFragment.kt
@@ -1,6 +1,7 @@
 package com.dark0ghost.math_for_kids.ui.dashboard
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,14 +10,24 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.dark0ghost.math_for_kids.databinding.FragmentDashboardBinding
+import com.dark0ghost.math_for_kids.score.Score
 import org.dark0ghost.math_emaple_generator.MathGenerate
 import org.dark0ghost.math_emaple_generator.MathOperation
+import java.math.BigDecimal
+
 
 class DashboardFragment : Fragment() {
 
     private lateinit var dashboardViewModel: DashboardViewModel
+
     private lateinit var editText: EditText
+
+    private lateinit var scoreView: TextView
+
+    private lateinit var mathExample: Pair<String, BigDecimal>
+
     private var _binding: FragmentDashboardBinding? = null
+
     private val mathGen: MathGenerate = MathGenerate()
 
     // This property is only valid between onCreateView and
@@ -32,27 +43,31 @@ class DashboardFragment : Fragment() {
                 ViewModelProvider(this).get(DashboardViewModel::class.java)
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         editText = binding.answer
+        scoreView = binding.score
         val root: View = binding.root
-
         val textView: TextView = binding.textDashboard
-       /* dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })*/
-        val result = mathGen.getData(listOf(MathOperation.Division,MathOperation.Multiplication),-100,1000,1)
-                /*editText.setOnEditorActionListener { v, actionId, event ->
-            return@setOnEditorActionListener when (actionId) {
-                EditorInfo.IME_ACTION_SEND -> {
-
-                    true
-                }
-                else -> false
+        val mathOperation = listOf(MathOperation.Division,MathOperation.Multiplication)
+        mathExample = mathGen.getData(mathOperation,5,25,1)
+        scoreView.text = Score.score.toString()
+        editText.setOnEditorActionListener {
+            v, actionId, event ->
+            Log.d("edittext",editText.text.toString())
+            if(editText.text.toString() == mathExample.second.toString()){
+                Score.updateScore()
+                scoreView.text = Score.score.toString()
+                mathExample = mathGen.getData(mathOperation,5,25,1)
+                textView.text = mathExample.first +" ${mathExample.second}"
+                editText.text.clear()
+                return@setOnEditorActionListener true
             }
-        }*/
-        textView.text = result.first +" ${result.second}"
+            return@setOnEditorActionListener false
+        }
+        textView.text = mathExample.first +" ${mathExample.second}"
         return root
     }
 
     override fun onDestroyView() {
+        Score.save()
         super.onDestroyView()
         _binding = null
     }
