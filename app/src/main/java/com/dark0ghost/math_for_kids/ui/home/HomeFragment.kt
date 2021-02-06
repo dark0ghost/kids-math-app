87b35f3df42802fc2b0e82544ed953ce4c1f76d1
dark0ghost <dark0ghostworkmail@seznam.cz>
2021-02-06 22:26:07 +0300

    intarface

--- app/src/main/java/com/dark0ghost/math_for_kids/ui/home/HomeFragment.kt
+++ app/src/main/java/com/dark0ghost/math_for_kids/ui/home/HomeFragment.kt
@@ -7,8 +7,6 @@ import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.dark0ghost.math_for_kids.R
 import com.dark0ghost.math_for_kids.databinding.FragmentHomeBinding
 import com.dark0ghost.math_for_kids.score.Score
@@ -20,9 +18,6 @@ class HomeFragment : Fragment() {
     private lateinit var editText: EditText
 
     private var _binding: FragmentHomeBinding? = null
-
-
-
 
     // This property is only valid between onCreateView and
     // onDestroyView.
