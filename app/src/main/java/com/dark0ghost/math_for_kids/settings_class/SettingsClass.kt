0a70b0bfd477bef0af856694da839727ad978de9
dark0ghost <dark0ghostworkmail@seznam.cz>
2021-02-06 22:50:32 +0300

    intarface

--- app/src/main/java/com/dark0ghost/math_for_kids/settings_class/SettingsClass.kt
+++ app/src/main/java/com/dark0ghost/math_for_kids/settings_class/SettingsClass.kt
@@ -3,7 +3,9 @@ package com.dark0ghost.math_for_kids.settings_class
 import android.content.SharedPreferences
 
 interface SettingsClass {
+
     fun save(preferences: SharedPreferences)
 
     fun init(preferences: SharedPreferences)
+
 }
